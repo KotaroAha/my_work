b3215b4985038017f02215fd46de8fe31de8accb
Kotaro Inami <kinami@c1r22s3.42tokyo.jp>
2023-06-09 19:07:41 +0900

    aa

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: kinami <kinami@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 17:22:41 by kinami            #+#    #+#             */
-/*   Updated: 2023/06/08 18:56:06 by kinami           ###   ########.fr       */
+/*   Updated: 2023/06/09 18:35:52 by kinami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_atoi(const char *str)
 {
-	int	num;
+	long long	num	;
 	int	sign;
 
 	num = 0;
@@ -28,15 +28,16 @@ int	ft_atoi(const char *str)
 		if (*str == '+' && *str == '-')
 			return (0);
 	}
-	while (*str)
+	while (ft_isdigit(*str))
 	{
-		if (*str >= '0' && *str <= '9')
+		if (num > LONG_MAX / 10)
 		{
-			num = num * 10 + (*str - '0');
-			str++;
-		}
-		else
+			num = LONG_MAX;
 			break;
+		}
+		num = num * 10 + (*str - '0');
+		str++;
 	}
-	return (num * sign);
+
+	return (int)(num * sign);
 }
--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: kinami <kinami@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 18:56:11 by kinami            #+#    #+#             */
-/*   Updated: 2023/06/08 19:25:39 by kinami           ###   ########.fr       */
+/*   Updated: 2023/06/09 16:45:37 by kinami           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	size_t	total_size;
 	void	*ptr;
-
+	
+	if (count >= SIZE_MAX / size)
+		return (NULL);
 	total_size = count * size;
 	ptr = malloc(total_size);
-	if (!ptr || count >= SIZE_MAX / size)
+	if (!ptr)
 		return (NULL);
 	if (ptr != NULL)
 		ft_bzero(ptr, total_size);
